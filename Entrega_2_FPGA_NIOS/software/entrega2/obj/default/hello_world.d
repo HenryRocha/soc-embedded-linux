f12d6c8ca27ce6c20a693b57e8ca0d0d26f3a837
Henry Rocha <henryfr1@al.insper.edu.br>
2020-09-21 18:17:36 -0300

    Add entrega 2 files

---
+++ Entrega_2_FPGA_NIOS/software/entrega2/obj/default/hello_world.d
@@ -0,0 +1,13 @@
+obj/default/hello_world.o: hello_world.c ../entrega2_bsp/system.h \
+ ../entrega2_bsp/linker.h ../entrega2_bsp//HAL/inc/alt_types.h \
+ ../entrega2_bsp//HAL/inc/io.h ../entrega2_bsp//HAL/inc/alt_types.h
+
+../entrega2_bsp/system.h:
+
+../entrega2_bsp/linker.h:
+
+../entrega2_bsp//HAL/inc/alt_types.h:
+
+../entrega2_bsp//HAL/inc/io.h:
+
+../entrega2_bsp//HAL/inc/alt_types.h:
