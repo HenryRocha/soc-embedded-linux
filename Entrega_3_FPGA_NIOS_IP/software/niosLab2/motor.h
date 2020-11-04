3546f0499052e5c8ad9e091cb99d4165dd7895e1
Henry Rocha <henryrocha@protonmail.com>
2020-11-04 20:48:06 -0300

    Refactor motor.h, not working still

--- Entrega_3_FPGA_NIOS_IP/software/niosLab2/motor.h
+++ Entrega_3_FPGA_NIOS_IP/software/niosLab2/motor.h
@@ -5,44 +5,44 @@
 void motor_init(unsigned int *p_motor) {
   // Recebe um ponteiro para o endereco do motor.
   // Inicializa o motor.
-  printf("Motor init: %d\n", *(p_motor));
-  *(p_motor) = 0x0001;
-  printf("Motor init After: %d\n", *(p_motor));
-  printf("Valor lido EN: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Valor lido EN before: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Escrevendo no EN\n");
+  IOWR_32DIRECT(p_motor, 0, 1);
+  printf("Valor lido EN after: %d\n", IORD_32DIRECT(p_motor, 0));
 }
 
 void motor_halt(unsigned int *p_motor) {
   // Recebe um ponteiro para o endereco do motor.
   // Para o motor.
-  printf("Motor Halt: %d\n", *(p_motor));
-  *(p_motor) = 0x0000;
-  printf("Motor Halt After: %d\n", *(p_motor));
-  printf("Valor lido EN: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Valor lido EN before: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Escrevendo no EN\n");
+  IOWR_32DIRECT(p_motor, 0, 0);
+  printf("Valor lido EN after: %d\n", IORD_32DIRECT(p_motor, 0));
 }
 
 void motor_en(unsigned int *p_motor) {
   // Recebe um ponteiro para o endereco do motor.
   // Liga o motor.
-  printf("Motor Enable: %d\n", *(p_motor));
-  *(p_motor) = 0x0001;
-  printf("Motor Enable After: %d\n", *(p_motor));
-  printf("Valor lido EN: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Valor lido EN before: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Escrevendo no EN\n");
+  IOWR_32DIRECT(p_motor, 0, 1);
+  printf("Valor lido EN after: %d\n", IORD_32DIRECT(p_motor, 0));
 }
 
 void motor_dir(unsigned int *p_motor, int dir) {
   // Recebe um ponteiro para o endereco do motor e a direcao desejada.
   // Muda a direcao do motor.
-  printf("Motor Dir: %d\n", *(p_motor));
-  *(p_motor) = dir;
-  printf("Motor Dir After: %d\n", *(p_motor));
-  printf("Valor lido DIR: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Valor lido DIR before: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Escrevendo no DIR\n");
+  IOWR_32DIRECT(p_motor, 0, dir);
+  printf("Valor lido DIR after: %d\n", IORD_32DIRECT(p_motor, 0));
 }
 
 void motor_vel(unsigned int *p_motor, int vel){
   // Recebe um ponteiro para o endereco do motor e a velocidade desejada.
   // Muda a velocidade do motor.
-  printf("Motor Vel: %d\n", *(p_motor));
-  *(p_motor) = vel;
-  printf("Motor Vel After: %d\n", *(p_motor));
-  printf("Valor lido VEL: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Valor lido VEL before: %d\n", IORD_32DIRECT(p_motor, 0));
+  printf("Escrevendo no VEL\n");
+  IOWR_32DIRECT(p_motor, 0, vel);
+  printf("Valor lido VEL after: %d\n", IORD_32DIRECT(p_motor, 0));
 }
